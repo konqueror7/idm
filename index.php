5d925e7bc21146708bca9eced9daec914fad1091
konqueror7 <konqueror7@gmail.com>
2021-03-31 05:26:48 +0500

    Commit #2 branch idm/twig fix html, month and day templates

--- index.php
+++ index.php
@@ -21,59 +21,10 @@ if (!$db->connect->error) {
   if (!isset($_GET['day'])) {
     $query = New MonthSelectQueryData();
     $dataTableMonth = $query->queryToDataBase(Array("name"=>$_POST["name"], "month"=>$_POST["month"]), $db);
-
-    //
-    // echo '<pre>';
-    // var_dump($dataTable);
-    // echo '</pre>';
-    // foreach ($dataTable as $tableRow) {
-    //   $table_row = '';
-    //   foreach ($tableRow as $key => $column) {
-    //     if ($key=='Work_shift' && $column != 'Total') {
-    //       $table_row .= '<td><a href="?day='.$column.'">'.$column.'</a></td>';
-    //     } else {
-    //       $table_row .= '<td>'.$column.'</td>';
-    //     }
-    //   }
-    //   $table_rows .= '<tr>'.$table_row.'</tr>';
-    // }
-    // $table_head = '  <tr>
-    // <th>Смена</th>
-    // <th>Начало смены</th>
-    // <th>Конец смены</th>
-    // <th>Заезд</th>
-    // <th>Генеральная уборка</th>
-    // <th>Текущая уборка</th>
-    // <th>Сумма</th>
-    // <tr>';
   } else {
     $query = New DaySelectQueryData();
     $dataTableDay = $query->queryToDataBase(Array("name"=>$_POST["name"], "day"=>$_GET["day"]), $db);
-    // foreach ($dataTable as $tableRow) {
-    //   $table_row = '';
-    //   foreach ($tableRow as $key => $column) {
-    //     if ($column !='' || !is_null($column)) {
-    //       $table_row .= '<td>'.$column.'</td>';
-    //     } else {
-    //       $table_row .= '<td> Нет данных </td>';
-    //     }
-    //   }
-    //   $table_rows .= '<tr>'.$table_row.'</tr>';
-    // }
-    // $table_head = '  <tr>
-    // <th>Номер (корпус)</th>
-    // <th>Тип номера</th>
-    // <th>Тип уборки</th>
-    // <th>Начало уборки</th>
-    // <th>Конец уборки</th>
-    // <th>Оплата</th>
-    // <tr>';
   }
-
-  // $table = '<table class="datasheet">'
-  //           .$table_head
-  //           .$table_rows.
-  //           '</table>';
 }
 
 $db->connect->close();
